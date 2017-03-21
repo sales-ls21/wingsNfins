088c8a369fecdccd9fa3c1fc600623de9c730b58
LaDonna <boxgirl85@gmail.com>
2017-03-20 22:43:10 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+/*
+a Pen by DIACO : http://codepen.io/MAW
+powered by GSAP : http://www.greensock.com/
+*/
+var tl = new TimelineMax({paused:true, onComplete:remove})  
+tl.to("#d2",1.5,{x:-500,ease:Sine.easeInOut})
+  .to("#d1",1.5,{x:500,ease:Sine.easeInOut},0)
+  .to(".clicker",0.5,{autoAlpha:0},0)
+  .set("#gate",{pointerEvents:'none'},1)
+  
+$(".clicker").click(function(){ 
+  tl.play(); 
+});
+   
+function remove(){
+  $(".svg-container").empty();
+}
