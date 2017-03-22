49ea1e88d1bec8a937d772c3c803527d84b43c0b
LaDonna <boxgirl85@gmail.com>
2017-03-21 21:46:51 -0500

    markup updated

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 a Pen by DIACO : http://codepen.io/MAW
 powered by GSAP : http://www.greensock.com/
 */
+$("#logoarea").hide();
+
 var tl = new TimelineMax({paused:true, onComplete:remove})  
 tl.to("#d2",1.5,{x:-500,ease:Sine.easeInOut})
   .to("#d1",1.5,{x:500,ease:Sine.easeInOut},0)
@@ -9,9 +11,10 @@ tl.to("#d2",1.5,{x:-500,ease:Sine.easeInOut})
   .set("#gate",{pointerEvents:'none'},1)
   
 $(".clicker").click(function(){ 
-  tl.play(); 
+  tl.play();
 });
    
 function remove(){
   $(".svg-container").empty();
+  $("#logoarea").show();
 }
